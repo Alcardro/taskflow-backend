ade8955d9b1cc8bf99151ce6369b4fc15ca99e56
Carlos Alejandro <27aniconsol@gmail.com>
2026-05-30 21:03:45 -0500

    feat: crear entidad Task y repositorio JPA

---
+++ src/main/java/com/taskflow_backend/domain/Task.java
@@ -0,0 +1,82 @@
+package com.taskflow_backend.domain;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    //ESTA NOTACION INDICA QUE ESTE CAMPO NO PUEDE SER FALSO EN LA TABLA
+    @Column(nullable = false)
+    private Boolean completed = false;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    //CONSTRUCTOR VACIO OBLIGATORIO PARA JPA
+    public Task() {
+    }
+
+    public Task(Long id, String title, String description, Boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    //Metodo que se ejecuta antes de persistir por primera vez
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt=LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/taskflow_backend/repository/infrastructure/TaskRepository.java
@@ -0,0 +1,10 @@
+package com.taskflow_backend.repository.infrastructure;
+
+import com.taskflow_backend.domain.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task,Long> {
+
+}
